8da073fa0cc8986e041acef4a95ba14a7daf6813
Pablo Cortés Sánchez <69325902+pablocort@users.noreply.github.com>
2023-09-06 15:40:33 -0500

    Add files via upload

---
+++ maps_stream.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+
+# Set the app title
+st.title("Retos de trayectoria")
+
+# Display the text
+st.write("Esta es una aplicación de datos simple que muestra 'Retos de trayectoria'.")
+
+
+
+
