bb2967b4855370d4b3e47aebcef703897a75980e
Pablo <69325902+pablocort@users.noreply.github.com>
2023-09-08 15:15:32 -0500

    Update maps_stream.py

--- maps_stream.py
+++ maps_stream.py
@@ -4,7 +4,7 @@ import pandas as pd
 import folium
 from folium.plugins import HeatMap, FastMarkerCluster
 from streamlit_folium import st_folium
-#import sys
+import sys
 #st.write(sys.executable)
 
 st.write("""
@@ -15,6 +15,7 @@ Mapa de concetración de número de estudiantes por equipo de cómputo
 """)
 
 sedes_tic = pd.read_csv('data/sedes_geo.csv', sep = '|')
+
 
 
 def generateBaseMap(default_location=[4.631530, -74.109180], default_zoom_start=11):
