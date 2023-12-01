e6bdbe5dfa4354157e00b91990ad83491cf9301f
Pablo <69325902+pablocort@users.noreply.github.com>
2023-12-01 17:36:16 -0500

    Update maps_stream.py

--- maps_stream.py
+++ maps_stream.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import zipfile
-import plotly.graph_objects as go  # Importing graph_objects from plotly
-import plotly as px
-
-
+import plotly.graph_objects as go
 
 def load_data():
     zip_file_path = 'data/icfes_performance.zip'
@@ -15,31 +12,15 @@ def load_data():
             icfes = pd.read_csv(file, delimiter='|')
     return icfes
 
-
-
 def create_stacked_bar_plot(data, custom_palette):
-    # Convert the custom_palette to the Plotly color format
-    custom_palette_plotly = [
-        f'rgb({int(color[0] * 255)},{int(color[1] * 255)},{int(color[2] * 255)})'
-        for color in custom_palette
-    ]
-    selected_columns = ['Matemáticas', 
-                    'Sociales',
-                    'Ciencias naturales', 
-                    'Lectura crítica']
-    mean_percentages = []
-    for column in selected_columns:
-        percentages = data[column].value_counts(normalize=True) * 100
-        mean_percentages.append(percentages)
+    custom_palette_plotly = [f'rgb({int(color[0] * 255)},{int(color[1] * 255)},{int(color[2] * 255)})' for color in custom_palette]
+    
+    selected_columns = ['Matemáticas', 'Sociales', 'Ciencias naturales', 'Lectura crítica']
+    mean_percentages = [data[column].value_counts(normalize=True) * 100 for column in selected_columns]
 
-# Create a DataFrame for the stacked bar plot
     mean_percentages_df = pd.DataFrame(mean_percentages, index=selected_columns)
-    mean_percentages_df = mean_percentages_df[[1,2,3,4]]
+    mean_percentages_df = mean_percentages_df[[1, 2, 3, 4]].transpose()
 
-    # Transpose the DataFrame for horizontal bars
-    mean_percentages_df = mean_percentages_df.transpose()
-
-    # Create a horizontal stacked bar plot for mean percentages using Plotly
     fig = go.Figure()
 
     for column in mean_percentages_df.columns:
@@ -49,19 +30,17 @@ def create_stacked_bar_plot(data, custom_palette):
             text=[f'{value:.1f}%' for value in mean_percentages_df[column]],
             hoverinfo='text',
             name=f'Nivel {column}',
-            marker=dict(color=custom_palette_plotly[int(column)-1]),
+            marker=dict(color=custom_palette_plotly[int(column) - 1]),
         ))
 
-    # Customize the layout
     fig.update_layout(
         title='',
         xaxis_title='Áreas temáticas',
         yaxis_title='Participación porcentual',
         barmode='stack',
-        showlegend=True,  # Set to True to show the legend
+        showlegend=True,
     )
 
-    # Add legend titles for each Nivel
     for index, color in zip(mean_percentages_df.columns, custom_palette_plotly):
         fig.add_trace(go.Scatter(
             x=[],
@@ -74,20 +53,20 @@ def create_stacked_bar_plot(data, custom_palette):
 
     return fig
 
-
-
 def create_top_30_institutions_table(filtered_data, title="Top 30 Institutions with Highest Median Scores:"):
-    top_30_institutions = filtered_data[filtered_data['Matemáticas'] == 1]
-    top_30_institutions = top_30_institutions.groupby('cole_nombre_establecimiento')['punt_matematicas'].\
-        median().reset_index().sort_values(by=['punt_matematicas'], ascending=True)
-    top_30_institutions = top_30_institutions.head(30)
+    top_30_institutions = (
+        filtered_data[filtered_data['Matemáticas'] == 1]
+        .groupby('cole_nombre_establecimiento')['punt_matematicas']
+        .median()
+        .reset_index()
+        .sort_values(by=['punt_matematicas'], ascending=True)
+        .head(30)
+    )
 
-    # Custom column names
     custom_column_names = {'cole_nombre_establecimiento': 'Nombre de la institución', 'punt_matematicas': 'Puntaje mediano en matemáticas'}
-
+    
     st.write(f'## {title}')
     
-    # Rename columns
     top_30_institutions.rename(columns=custom_column_names, inplace=True)
     
     table_styles = [
@@ -96,7 +75,6 @@ def create_top_30_institutions_table(filtered_data, title="Top 30 Institutions w
     ]
 
     st.table(top_30_institutions.set_index('Nombre de la institución'))
-
 
 # Load data
 icfes = load_data()
@@ -129,15 +107,11 @@ else:
     else:
         filtered_data = icfes[(icfes['Departamento'] == selected_city) & (icfes['Municipio'] == selected_municipio)]
 
-
-
 # Select the columns for the variables you want to analyze
 selected_columns = ['Matemáticas', 'Sociales', 'Ciencias naturales', 'Lectura crítica']
 
 # Custom color palette
 custom_palette = [(0/255, 47/255, 135/255), (121/255, 163/255, 220/255), (232/255, 114/255, 0/255), (245/255, 179/255, 53/255)]
-
-
 
 # Use custom CSS to widen the layout
 st.markdown(
@@ -150,7 +124,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
 
 # Create a layout with two columns
 col1, col2 = st.columns(2)
@@ -165,5 +138,4 @@ st.markdown("&nbsp;")
 # In the second column, display the top 30 institutions table
 with col2:
     create_top_30_institutions_table(filtered_data, title="Custom Title for Top 30 Institutions:")
-
 
